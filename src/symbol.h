ff21f6885370065fef6baf0c9caad21efb787213
Ratchet W <3170101502@zju.edu.cn>
2020-05-30 00:50:39 +0800

    [UPDATE] symbol class definition

--- src/symbol.h
+++ src/symbol.h
@@ -1,21 +1,26 @@
 #include <iostream>
-
+#include <unordered_map>
 using namespace std;
 
 class Symbol
 {
 private:
-    string name;
+    string id;
+    // int scope;
+    static unordered_map<string, Symbol> dict;
 
 public:
-    Symbol(/* args */);
-    ~Symbol();
+    Symbol(string s) : id(s) {}
+    Symbol symbol(string s)
+    {
+        Symbol sym;
+        if (dict.find(s) != dict.end())
+        {
+            sym = dict[s];
+        }
+        else
+            sym = Symbol(s);
+        dict.insert({s, sym});
+        return sym;
+    }
 };
-
-Symbol::Symbol(/* args */)
-{
-}
-
-Symbol::~Symbol()
-{
-}
